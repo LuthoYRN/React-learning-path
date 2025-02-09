ac80aa583c4477338ed6f5d78694296f49f577d3
Lutho <luthoyrn@gmail.com>
2025-02-09 03:32:36 +0200

    Conditional rendering example

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
 const App = () => {
-    const [latitude, setLatitude] = React.useState(null);
-    useEffect(()=>{
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                setLatitude(position.coords.latitude)
-            },
-            (error) => console.log(error)
-        );
-    },[latitude])
-  return <div>latitude: {latitude}</div>;
+  const [latitude, setLatitude] = React.useState(null);
+  const [errorMessage, setErrorMessage] = React.useState("");
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLatitude(position.coords.latitude);
+      },
+      (error) => setErrorMessage(error.message)
+    );
+  }, [latitude]);
+  return (
+    <div>
+      {errorMessage && !latitude
+        ?  errorMessage 
+        : !errorMessage && latitude
+        ? latitude
+        : "Loading..."}
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
