53bab00c5030d16016e35d78fb4075b529940b01
Lutho <luthoyrn@gmail.com>
2025-02-09 03:16:18 +0200

    Example usage of useEffectand useState hooks.

---
+++ src/Hemisphere.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Hemisphere=()=>{
+
+}
+export default Hemisphere;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
 
-const App = () =>{
-    return(
-        <div>
-            <label htmlFor="email" className = "label">enter e-mail</label>
-            <input type="email" id="email" name="email"/>
-            <button style={{backgroundColor:'blue',color:'white'}} type="submit">Submit</button>
-        </div>
-    )
-}
+const App = () => {
+    const [latitude, setLatitude] = React.useState(null);
+    useEffect(()=>{
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                setLatitude(position.coords.latitude)
+            },
+            (error) => console.log(error)
+        );
+    },[latitude])
+  return <div>latitude: {latitude}</div>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<React.StrictMode>
+root.render(
+  <React.StrictMode>
     <App />
-</React.StrictMode>)
+  </React.StrictMode>
+);
